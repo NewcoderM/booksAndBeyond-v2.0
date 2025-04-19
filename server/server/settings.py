49c91d1e2d3f60b9aa6a3329b519c288c6b13ba1
NewcoderM <8m8opqt7@students.codeinstitute.net>
2025-04-19 12:07:18 +0200

    change static link for deployment

--- server/server/settings.py
+++ server/server/settings.py
@@ -58,7 +58,7 @@ SESSION_COOKIE_SECURE = True
 # Static files
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'client/build/static')]
 
 # Port
 PORT = os.getenv("PORT", "8080")
